78416cfa94dfc67d20f0218a2c8475ee8ec7f073
Mick <suncombo@gmail.com>
2017-04-05 21:06:38 +0800

    家入子模組前commit

--- start_for_pycharm.bat
+++
@@ -1,6 +0,0 @@
-title GreenOdoo - www.GreenOdoo.com
-COLOR 0A
-SET NODE_PATH="%CD%"\runtime\win32\bin\node_modules
-SET PATH="%CD%"\runtime\pgsql\bin;"%CD%"\runtime\python;"%CD%"\runtime\win32\lessc;"%CD%"\runtime\win32\wkhtmltopdf;%PATH%.
-"%CD%"\runtime\pgsql\bin\pg_ctl -D "%CD%"\..\..\postgresql_data\data -l "%CD%"\..\..\postgresql_data\logfile start
-REM "%CD%"\runtime\python\python-oe "%CD%"\source\odoo-bin -c "%CD%"\openerp-server.conf
